cbe47e493d7c2d7601b3a7ae72ba71eee267fdd6
vigneshj04 <vigneshjmax@gmail.com>
2022-04-24 12:25:38 +0530

    Added: Validating Password Using By Lambda Expression

--- UserDetails.java
+++ UserDetails.java
@@ -49,7 +49,7 @@ public class UserDetails {
             if (password.isEmpty()) {
                 throw new CustomException("Invalid input");
             }
-            Pattern pattern = Pattern.compile("^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$");
+            Pattern pattern = Pattern.compile("(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]*[@#$%_!^&*][0-9a-zA-Z]*");
             return pattern.matcher(password).matches();
         };
         System.out.println(validatePassword.validate("TamilNadu@22"));
