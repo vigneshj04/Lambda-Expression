5cea6cf3d8373acb101ffe6ca7ad83a3c129c176
vigneshj04 <vigneshjmax@gmail.com>
2022-04-24 12:06:02 +0530

    Added: Lambda Programs

---
+++ CustomException.java
@@ -0,0 +1,7 @@
+package com.Lambda;
+
+public class CustomException extends Exception {
+    CustomException(String msg) {
+        super(msg);
+    }
+}
---
+++ UserDetails.java
@@ -0,0 +1,11 @@
+package com.Lambda;
+
+import java.util.regex.Pattern;
+
+
+
+ public class UserDetails {
+     public static void main(String[] args) throws CustomException {
+
+     }
+ }
