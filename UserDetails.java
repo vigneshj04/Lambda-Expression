87ca60d2503da4643721a277ed5da9899cf3cf68
vigneshj04 <vigneshjmax@gmail.com>
2022-04-24 12:08:07 +0530

    Added: Validating First Name Using Lambda Expression

--- UserDetails.java
+++ UserDetails.java
@@ -2,10 +2,22 @@ package com.Lambda;
 
 import java.util.regex.Pattern;
 
+@FunctionalInterface
+interface UserInterface {
+    boolean validate(String str) throws CustomException;
+}
+
+public class UserDetails {
+    public static void main(String[] args) throws CustomException {
+        UserInterface validateFirstName = (firstName) -> {
+            if (firstName.isEmpty()) {
+                throw new CustomException("Invalid input");
+            }
+            Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+            return pattern.matcher(firstName).matches();
+        };
+        System.out.println(validateFirstName.validate("Rajesh"));
 
 
- public class UserDetails {
-     public static void main(String[] args) throws CustomException {
-
-     }
- }
+    }
+}
